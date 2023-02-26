31e128bc44c701a5cd7978450ecc546488f57c76
Sit, Johnny <johnny.sit@optum.com>
2023-02-26 13:17:22 -0500

    feat: cloned hacking with swift repo

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Sit, Johnny on 2/26/23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
